df8deade691cc57b30e171225d171a4915878f30
mangal <mangalhcl449@gmail.com>
2016-07-08 18:10:19 +0530

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,27 @@
+MAIL_DRIVER=mail
+MAIL_HOST=mailtrap.io
+MAIL_PORT=2525
+MAIL_USERNAME=mangalhcl449@gmail.com
+MAIL_PASSWORD=meanstack99
+MAIL_ENCRYPTION=tls
+MAIL_PRETEND=false
+
+
+
+// Route::get('sendemail', function () {
+
+//     $data = array(
+//         'name' => "Mangal",
+//     );
+
+//     Mail::send('succefull', $data, function ($message) {
+
+//        // $message->from('mangalhcl449@gmail.com', 'Learning Laravel');
+
+//         $message->to('mangal.singh@mobilyte.com')->subject('Learning Laravel test email');
+
+//     });
+
+//     return "Your email has been sent successfully";
+
+// });
